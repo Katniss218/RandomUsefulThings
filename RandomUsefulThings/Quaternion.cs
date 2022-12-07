12d56cf82b3bac6f3f91f80fc57641dd160e52fb
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2022-12-07 19:08:16 +0100

    yes

---
+++ RandomUsefulThings/Quaternion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomUsefulThings
+{
+    public class Quaternion
+    {
+        public float x, y, z, w;
+
+        public Quaternion( float x, float y, float z, float w )
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        public static Quaternion operator *( Quaternion q1, Quaternion q2 )
+        {
+            return new Quaternion(
+                q1.w * q2.x + q1.x * q2.w + q1.y * q2.z - q1.z * q2.y,
+                q1.w * q2.y + q1.y * q2.w + q1.z * q2.x - q1.x * q2.z,
+                q1.w * q2.z + q1.z * q2.w + q1.x * q2.y - q1.y * q2.x,
+                q1.w * q2.w - q1.x * q2.x - q1.y * q2.y - q1.z * q2.z );
+        }
+    }
+}
---
+++ RandomUsefulThings/Vector3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomUsefulThings
+{
+    public class Vector3
+    {
+        public float x, y, z;
+
+        public Vector3( float x, float y, float z )
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        // rotate a vector by a quaternion (assuming origin is (0,0,0) ??)
+        public static Vector3 operator *( Quaternion q, Vector3 v )
+        {
+            Quaternion vq = new Quaternion( v.x, v.y, v.z, 0 );
+            Quaternion r = q * vq * q.Inverse();
+            return new Vector3( r.x, r.y, r.z );
+        }
+        
+        // cross-product
+        public static Quaternion operator *( Vector3 v1, Vector3 v2 )
+        {
+            return new Quaternion(
+                v1.y * v2.z - v1.z * v2.y,
+                v1.z * v2.x - v1.x * v2.z,
+                v1.x * v2.y - v1.y * v2.x,
+                0 );
+        }
+
+        // One of the main uses for this is getting the rotation between two rotations.
+        // For example if you wanted to know the Quaternion that would get you from rotationA to rotationB you would do something like this:
+        /// FromAtoB = Quaternion.Inverse(rotationA) * rotationB
+    }
+}
