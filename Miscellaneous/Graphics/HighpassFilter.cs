5065d9a1e3555799fc1fc1e879382f39b43ddb11
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2023-03-31 23:05:59 +0200

    yes

---
+++ Miscellaneous/Graphics/HighpassFilter.cs
@@ -0,0 +1,70 @@
+﻿using RandomUsefulThings.Math.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomUsefulThings.Misc.Graphics
+{
+    public static class HighpassFilter
+    {
+        public static Matrix Apply( Matrix input, double radius )
+        {
+            int kernelSize = (int)System.Math.Round( radius * 3.0 ) * 2 + 1;
+
+            Matrix kernel = new Matrix( NormalizeKernel( CreateGaussianKernel( kernelSize, radius )) );
+
+            Matrix blurred = Matrix.Convolve( input, kernel );
+
+            // Lowpass should be without the subtraction (just blur) I think.
+            Matrix output = Matrix.SubtractElementwise( input, blurred );
+
+            return output;
+        }
+
+        // Helper method to create a Gaussian kernel with the specified kernel size and standard deviation
+        private static double[,] CreateGaussianKernel( int kernelSize, double sigma )
+        {
+            double[,] kernel = new double[kernelSize, kernelSize];
+
+            int center = kernelSize / 2;
+
+            // Compute the kernel weights based on the Gaussian distribution
+            for( int i = 0; i < kernelSize; i++ )
+            {
+                for( int j = 0; j < kernelSize; j++ )
+                {
+                    double x = i - center;
+                    double y = j - center;
+                    kernel[i, j] = System.Math.Exp( -(x * x + y * y) / (2 * sigma * sigma) );
+                }
+            }
+
+            return kernel;
+        }
+
+        // Helper method to normalize a kernel so that all weights sum to 1
+        private static double[,] NormalizeKernel( double[,] kernel )
+        {
+            // Compute the sum of all kernel weights
+            double sum = 0.0;
+            for( int i = 0; i < kernel.GetLength( 0 ); i++ )
+            {
+                for( int j = 0; j < kernel.GetLength( 1 ); j++ )
+                {
+                    sum += kernel[i, j];
+                }
+            }
+
+            // Normalize the kernel weights by dividing each weight by the sum
+            for( int i = 0; i < kernel.GetLength( 0 ); i++ )
+            {
+                for( int j = 0; j < kernel.GetLength( 1 ); j++ )
+                {
+                    kernel[i, j] /= sum;
+                }
+            }
+
+            return kernel;
+        }
+    }
+}
---
+++ Miscellaneous/Graphics/SobelFilter.cs
@@ -0,0 +1,47 @@
+﻿using RandomUsefulThings.Math.LinearAlgebra;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomUsefulThings.Misc.Graphics
+{
+    public static class SobelFilter
+    {
+        [Obsolete("Unconfirmed")]
+        public static Matrix Apply( Matrix inputImage )
+        {
+            // Applies a sobel (edge detect) filter.
+            Matrix Kx = new Matrix( new double[,]
+                { {-1, 0, 1},
+                  {-2, 0, 2},
+                  {-1, 0, 1} } );
+
+            Matrix Ky = new Matrix( new double[,]
+                { {-1,-2,-1},
+                  { 0, 0, 0},
+                  { 1, 2, 1} } );
+
+            Matrix outputImage = new Matrix( inputImage.Rows, inputImage.Cols );
+
+            // Convolve the input image with the Kx and Ky kernels
+            Matrix gx = Matrix.Convolve( inputImage, Kx );
+            Matrix gy = Matrix.Convolve( inputImage, Ky );
+
+            // Compute the magnitude of the gradient at each pixel
+            for( int i = 0; i < inputImage.Rows; i++ )
+            {
+                for( int j = 0; j < inputImage.Cols; j++ )
+                {
+                    double gxVal = gx[i, j];
+                    double gyVal = gy[i, j];
+
+                    double gradientMagnitude = System.Math.Sqrt( gxVal * gxVal + gyVal * gyVal );
+
+                    outputImage[i, j] = gradientMagnitude;
+                }
+            }
+
+            return outputImage;
+        }
+    }
+}
